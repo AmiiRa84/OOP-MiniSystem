53847708f0b31bb3856f294bd15e9ee53726dee0
AmiiRa84 <amiraasaad90@gmail.com>
2025-08-14 21:02:36 +0300

    Add project files.

---
+++ Task/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task
+{
+    class Animal
+    {
+        //        1. Animal(Base Class)
+        //Should store basic information common to all animals(name, age).
+        //Must provide a way for derived classes to define their own way of making sounds.
+        //Must have a method to display animal details, but allow child classes to change how it works.
+        //Must allow feeding animals through a common action.  [feeding]
+
+        public string Name { get; set; }
+
+    }
+}
